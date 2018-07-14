cc3124b651a1632325202808e66c5d3b08aa9ba7
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 17:22:04 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -41,6 +41,6 @@ class TextAnalyzer
   end
 
   def used_this_many_times
-    text.split("").filter{|letter| letter == most_used_letter}.count
+    text.split("").select{|letter| letter == most_used_letter}.count
   end
 end
